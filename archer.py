9ae598b498102ad5cbaab0b4e81a0da72b47cd27
Andrew Eakett <aeakett@gmail.com>
2013-03-12 15:31:52 -0400

    now actually reads a config file

--- archer.py
+++ archer.py
@@ -2,11 +2,14 @@
 
 import ConfigParser, os
 
-config= None
+config = ConfigParser.RawConfigParser()
 for loc in os.curdir, os.path.expanduser("~"), os.environ.get("ARCHERRC"):
-    try:
-        with open(os.path.join(loc,".archerrc")) as source:
-            config.readfp( source )
-    except IOError:
-        pass
+   try:
+      with open(os.path.join(loc, ".archerrc")) as source:
+         config.readfp( source )
+   except (IOError, AttributeError) as e:
+      pass
 
+print config.get("archer", "webciteEmail")
+print config.get("archer", "archQueueFile")
+print config.get("archer", "outputPath")
